062fd06bf9a48751e7067e1c122a12a052cbeaf2
kyorer <khanhtnts00424@fpt.edu.vn>
2025-03-17 10:00:27 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/newsweb/', // Đúng tên repo GitHub của bạn
   plugins: [vue()],
   resolve: {
     alias: {
@@ -11,3 +10,4 @@ export default defineConfig({
     },
   },
 })
+
